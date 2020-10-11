483870bbf6c4f9e0a28cd583bc3b4672a964e686
Jesus Manteca <jesusmanteca@gmail.com>
2020-10-11 11:03:05 -0500

    added search by id functionality step one

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const fs = require('fs');
 const path = require('path');
 //require the data in order to create a route
-const { db } = require('./db/db');
+const { notes } = require('./db/db');
 
 
 const PORT = process.env.PORT || 3001;
@@ -14,12 +14,27 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
+function findById(id, notesArray) {
+    const result = notesArray.filter(note => note.id === id)[0];
+    return result;
+  }
+
 // add the GET routes
 app.get('/api/notes', (req, res) => {
     // res.send('<h1>Hello!</h1>');
     let notes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
     res.json(notes);
 });
+
+app.get('/api/notes/:id', (req, res) => {
+    const result = findById(req.params.id, notes);
+    if (result) {
+        res.json(result);
+    } else {
+        res.send(404);
+    }
+  });
+
 
 
 // method to make our server listen
